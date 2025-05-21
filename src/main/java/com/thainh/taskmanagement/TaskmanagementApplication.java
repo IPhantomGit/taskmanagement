956ea9fed6cefd7231a5512a3c3da11e2e8b9bfc
IPhantomGit <nguyenhongthai.0110@gmail.com>
2025-05-22 00:01:42 +0700

    initial project

---
+++ src/main/java/com/thainh/taskmanagement/TaskmanagementApplication.java
@@ -0,0 +1,27 @@
+package com.thainh.taskmanagement;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "TaskManagement API",
+				version = "1.0",
+				description = "TaskManagement API Documentation",
+				contact = @Contact(
+						name = "ThaiNH",
+						email = "nguyenhongthai.0110@gmail.com"
+				)
+		)
+)
+public class TaskmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thainh/taskmanagement/TaskmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thainh.taskmanagement;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
